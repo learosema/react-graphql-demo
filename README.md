1486f90624bef6ab1f9762b3aba57554720646f6
Lea Rosema <terabaud@gmail.com>
2018-08-18 11:20:42 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-graphql-demo
 Demo App using React and GraphQL
+
+Setup is like https://github.com/adhrinae/ts-react-parcel/ but from scratch.
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+/*
+export default function App() {
+  return (<h1>Hello World!</h1>)
+} */
+
+
+export default class App extends React.Component {
+  render() {
+    return (<h1>Hi World!</h1>)
+  }
+}
