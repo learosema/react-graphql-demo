2a228f732480cf0848944275d5fcddc989c8c253
Lea Rosema <terabaud@gmail.com>
2018-08-21 18:27:44 +0200

    move styles and install apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react'
 import { render } from 'react-dom'
-import './index.css'
+import './styles/index.css'
 
 import App from './components/App'
 
-render(<App />, document.getElementById('root'))
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({ uri: 'http://localhost:4000/ '})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+)
