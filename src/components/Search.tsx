da0ac478dac68779fa9ac66ee0ecebcf9575a58d
Lea Rosema <terabaud@gmail.com>
2018-08-29 23:53:26 +0200

    add search feature

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,30 @@ import * as React from 'react'
 import { withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
 
-import Link from './link'
+import Link from './Link'
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
 
 class Search extends React.Component<any, any> {
 
@@ -11,25 +34,33 @@ class Search extends React.Component<any, any> {
     filter: ''
   }
 
-  _executeSearch = async (e: React.FormEvent) => {
-    e.preventDefault()
+  _executeSearch = async () => {
+    const { filter } = this.state
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter }
+    })
+    const links = result.data.feed.links
+    this.setState({ links })
   }
 
   render() {
-    return (<div className="search-form">
-      <form onSubmit={this._executeSearch}>
+    return (<div className="form">
+      <form onSubmit={(e) => {
+          e.preventDefault()
+          this._executeSearch()
+        }}>
         <label>Search</label>
         <input type="text" onChange={e => this.setState({ filter: e.target.value })} />
         <button> OK </button>
       </form>
       <ol>
-        {this.state.links.map((link:any, index) => {
+        {this.state.links.map((link:any, index) => 
           <Link key={link.id} link={link} index={index} />
-        })}
+        )}
       </ol>
     </div>)
   }
-
 }
 
 export default withApollo(Search)
