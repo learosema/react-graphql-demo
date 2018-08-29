e9810d99db10c066ef8f26433db604eb50b0a001
Lea Rosema <terabaud@gmail.com>
2018-08-30 01:21:11 +0200

    TODO

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -26,6 +26,58 @@ export const FEED_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      node {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      node {
+        id
+        link {
+          id
+          url
+          description
+          createdAt
+          postedBy {
+            id
+            name
+          }
+          votes {
+            id
+            user {
+              id
+            }
+          }
+        }
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
 export default class LinkList extends React.Component<any, any> {
 
   _updateCacheAfterVote = (store: any, createVote: any, linkId: string) => {
@@ -35,17 +87,50 @@ export default class LinkList extends React.Component<any, any> {
     store.writeQuery({ query: FEED_QUERY, data })
   }
 
+  _subscribeToNewVotes = async (subscribeToMore: Function) => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
+  _subscribeToNewLinks = async (subscribeToMore : Function) => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev: any, { subscriptionData }: any) => {
+        console.log("updateQuery Fired!", subscriptionData)
+        if (!subscriptionData.data) return prev
+        if (!subscriptionData.data.newLink) {
+          console.log("TODO does not work yet :/", subscriptionData.data)
+          return prev
+        }
+        const newLink = subscriptionData.data.newLink
+        const newLinkNode = newLink.node
+  
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLinkNode, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
         {
-          ({loading, error, data}) => {
+          ({loading, error, data, subscribeToMore}) => {
             if (loading) {
               return <div>Loading</div>
             }
             if (error) {
               return <div>Error</div>
             }
+            this._subscribeToNewLinks(subscribeToMore)
+            this._subscribeToNewVotes(subscribeToMore)
+
             const linksToRender : Array<any> = data.feed.links
             return (<ol>
               {linksToRender.map((link, index) => 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import './styles/index.css'
-
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
-
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
-
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import { OperationDefinitionNode } from 'graphql';
 
 const httpLink = createHttpLink({ uri: 'http://localhost:4000/' })
 
@@ -23,10 +24,29 @@ const authLink = setContext((_, { headers }) => {
       authorization: token ? `Bearer ${token}` : ''
     }
   }
-}) 
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } : any = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
